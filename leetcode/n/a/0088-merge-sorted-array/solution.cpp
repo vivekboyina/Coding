0fbdc73351d7b3c5c2d70b02f7bf1068b4cba9dd
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-10 20:30:26 +0530

    ✅ Merge Sorted Array | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 12.4 MB | 2026-07-10

--- leetcode/n/a/0088-merge-sorted-array/solution.cpp
+++ leetcode/n/a/0088-merge-sorted-array/solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
-        int mn = m + n;
-        for(int i = m - 1; i >= 0; i--) n1[i + n] = n1[i];
-        for(int i = 0; i < n; i++) n1[i] = 0;
-        int i = n;
-        int j = 0;
-        int k = 0;
-        while(i < mn && j < n)
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0)
         {
-            if(n1[i] < n2[j]) n1[k++] = n1[i++];
-            else n1[k++] = n2[j++];
+            if(n1[i] > n2[j]) n1[k--] = n1[i--];
+            else n1[k--] = n2[j--];
         }
-        while(i < mn) n1[k++] = n1[i++];
-        while(j < n) n1[k++] = n2[j++];
+        while(j >= 0) n1[k--] = n2[j--];
     }
 };
