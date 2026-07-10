9ac78ac4148c62bea7e695231df8955ae06850d0
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-10 20:55:12 +0530

    ✅ Remove Duplicates from Sorted Array | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 22.5 MB | 2026-07-10

--- leetcode/n/a/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ leetcode/n/a/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -3,10 +3,9 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int j = 0;
         int n = nums.size();
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i < n; i++)
         {
-            if(i == 0) nums[j] = nums[i];
-            else if(nums[i] != nums[j]) nums[++j] = nums[i];
+            if(nums[i] != nums[j]) nums[++j] = nums[i];
         }
         return j + 1;
     }
