2ff81694db2d1a3714e440c08f2fface7247d307
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-15 20:54:31 +0530

    ✅ STRINGSHIFT | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/STRINGSHIFT/solution.cpp
+++ codechef/medium/STRINGSHIFT/solution.cpp
@@ -10,14 +10,18 @@ int main() {
 	    cin >> n;
 	    string s;
 	    cin >> s;
+	    string o = s;
 	    string k;
+	    string ans = s;
 	    for(int i = 0; i < s.length(); i++)
 	    {
 	        k = s;
 	        if(k[i] == 'z') k[i] = 'a';
 	        else k[i] = k[i] + 1;
+	        ans = min(ans,min(s,k));
 	        if(s > k) s = k;
+	        else s = o;
 	    }
-	    cout << s << endl;
+	    cout << ans << endl;
 	}
 }
