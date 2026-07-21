2365bdf984d2cadbd609e2eb51472cd337e13a8e
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-21 20:23:48 +0530

    ✅ Maximize Active Section with Trade I | 1729 | C++ | Runtime: 82 ms (42.79%) | Memory: 54.9 MB | 2026-07-21

--- leetcode/1729/3499-maximize-active-section-with-trade-i/solution.cpp
+++ leetcode/1729/3499-maximize-active-section-with-trade-i/solution.cpp
@@ -1,70 +1,34 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.length();
-        vector<int>pf(n,-1);
-        vector<int>sf(n,-1);
-        int one = -1;
-        bool z = false;
-        for(int i = n - 1; i >= 0; i--)
-        {
-            if(s[i] == '1') one = i;
-            else z = true;
-            sf[i] = one;
+        int ones=0,l=0,r=0;
+        for(char ch:s) if(ch == '1') ones++;
+        int ans = ones;
+        while(r < s.size() && s[r] == '1') {
+            r++;
         }
-        if(one == -1) return 0;
-        if(!z) return n;
-        one = -1;
-        for(int i = 0; i < n; i++)
-        {
-            if(s[i] == '1') one = i;
-            pf[i] = one;
-        }
-        int st = -1;
-        int nd = -1;
-        int dis = INT_MIN;
-        for(int i = 1; i < n - 1; i++)
-        {
-            if(s[i] == '1')
-            {
-                if(sf[i + 1] == -1 && pf[i - 1] == -1)
-                {
-                    st = 0;
-                    nd = n;
-                }
-                else if(sf[i + 1] != -1 && pf[i - 1] == -1)
-                {
-                    if(sf[i + 1] > dis)
-                    {
-                        dis = sf[i + 1];
-                        st = 0;
-                        nd = sf[i + 1];
-                    }
-                }
-                else if(sf[i + 1] == -1 && pf[i - 1] != -1)
-                {
-                    if(n - 1 - pf[i - 1] > dis)
-                    {
-                        dis = n - 1 - pf[i - 1];
-                        st = pf[i - 1];
-                        nd = n - 1;
-                    }
-                }
-                else
-                {
-                    if(sf[i + 1] - pf[i - 1] > dis)
-                    {
-                        dis = sf[i + 1] - pf[i - 1];
-                        st = pf[i - 1];
-                        nd = sf[i + 1];
-                    }
-                }
+        while(r < s.size()) {
+            int lzero=0,mones=0,rzero=0;
+            while(s[r] == '0') {
+                lzero++;
+                r++;
             }
+            while(s[r] == '1') {
+                mones++;
+                r++;
+            }
+            if(mones == 0) break;
+            int rstart = r;
+            while(s[r] == '0') {
+                rzero++;
+                r++;
+            }
+            if(lzero > 0 && rzero > 0) {
+                int win = lzero+mones+rzero;
+                ans = max(ans,ones-mones+win);
+            }
+            r = rstart;
         }
-        if(st != -1 && nd != -1) for(int i = st; i <= nd; i++) s[i] = '1';
-        int ans = 0;
-        for(char i : s) if(i == '1') ans+=1;
-        cout << s;
         return ans;
     }
 };
