19109cc730db60a0b6c9e459468b7c1c7fa2b360
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-24 21:00:06 +0530

    ✅ Binary Search | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 31.3 MB | 2026-07-24

--- leetcode/n/a/0704-binary-search/solution.cpp
+++ leetcode/n/a/0704-binary-search/solution.cpp
@@ -9,9 +9,7 @@ public:
             m = l + (h - l)/2;
             if(nums[m] > target) h = m - 1;
             else if(nums[m] < target) l = m + 1;
-            else return m;
-            cout << l << " " << h << " " << m << endl;
-        }
+            else return m;        }
         return -1;
     }
 };
