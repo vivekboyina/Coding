546de0d45ef56d8b8ecbc4368180c05495771291
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-28 15:34:15 +0530

    ✅ Find First and Last Position of Element in Sorted Array | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 17.5 MB | 2026-07-28

--- leetcode/n/a/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ leetcode/n/a/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -7,13 +7,15 @@ public:
         int mid;
         while(low <= high)
         {
-            cout << low << " " << high << endl;
             mid = low + (high - low)/2;
             if(nums[mid] == target)
             {
-                if(nums[low] == target && nums[high] == target) return {low,high};
-                else if(nums[low] == target) high = mid;
-                else low = mid + 1;
+                while(mid < n && nums[mid] == target) mid++;
+                high = mid - 1;
+                mid-=1;
+                while(mid >= 0 && nums[mid] == target) mid--;
+                low = mid + 1;
+                return {low,high};
             }
             else if(nums[mid] > target) high = mid - 1;
             else low = mid + 1;
