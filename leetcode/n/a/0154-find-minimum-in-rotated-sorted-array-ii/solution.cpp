e6adc538ec8cfb7256409f8aee6f7a5b070ae7cc
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-28 18:22:56 +0530

    ✅ Find Minimum in Rotated Sorted Array II | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 16.2 MB | 2026-07-28

--- leetcode/n/a/0154-find-minimum-in-rotated-sorted-array-ii/solution.cpp
+++ leetcode/n/a/0154-find-minimum-in-rotated-sorted-array-ii/solution.cpp
@@ -8,8 +8,9 @@ public:
         while(low <= high)
         {
             mid = low + (high - low)/2;
-            if(nums[low] > nums[mid]) high = mid - 1;
-            else low = mid + 1;
+            if(nums[mid] > nums[high]) low = mid + 1;
+            else if(nums[mid] == nums[high]) high-=1;
+            else if(nums[mid] < nums[high]) high = mid;
         }
         return nums[low];
     }
