5beab53aa4fd82f970748e186a55b11f108f55f7
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-28 08:00:26 +0530

    ✅ First Bad Version | N/A | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-07-28

--- leetcode/n/a/0278-first-bad-version/solution.cpp
+++ leetcode/n/a/0278-first-bad-version/solution.cpp
@@ -10,9 +10,9 @@ public:
         while(low <= high)
         {
             mid = low + (high - low)/2;
-            if(isBadVersion(mid)) return mid;
+            if(isBadVersion(mid)) high = mid - 1;
             else low = mid + 1;
         }
-        return n;
+        return low;
     }
 };
