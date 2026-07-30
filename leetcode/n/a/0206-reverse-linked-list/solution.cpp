34d7199289627b9b0e67d07d272c9fcd99d9c38c
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-30 20:22:34 +0530

    ✅ Reverse Linked List | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 13.5 MB | 2026-07-30

---
+++ leetcode/n/a/0206-reverse-linked-list/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* next;
+        ListNode* ans = nullptr;
+        while(curr != nullptr)
+        {
+            next = curr -> next;
+            curr -> next = ans;
+            ans = curr;
+            curr = next;
+        }
+        return ans;
+    }
+};
