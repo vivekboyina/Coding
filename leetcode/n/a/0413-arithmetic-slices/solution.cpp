973e08aa11cc291a800c8c5be13a5095c21d1430
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-06 11:35:12 +0530

    ✅ Arithmetic Slices | N/A | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-06

---
+++ leetcode/n/a/0413-arithmetic-slices/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>diff(n,0);
+        for(int i = 1; i < n; i++) diff[i] = nums[i] - nums[i - 1];
+        int ans = 0;
+        int cnt = 1;
+        int k;
+        for(int i = 1; i < n; i++)
+        {
+            if(diff[i - 1] == diff[i]) cnt++;
+            else cnt = 1;
+            cout << cnt << endl;
+            k = cnt - 1;
+            k = (k * (k + 1))/2;
+            ans = max(ans,k);
+        }
+        return ans;
+    }
+};
