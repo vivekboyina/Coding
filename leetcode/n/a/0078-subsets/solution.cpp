f50318bc99ab5283f3e147bc9c677d101b35847d
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-08 08:23:18 +0530

    ✅ Subsets | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 16.8 MB | 2026-08-08

--- leetcode/n/a/0078-subsets/solution.cpp
+++ leetcode/n/a/0078-subsets/solution.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    void rec(int i,vector<int>nums,vector<vector<int>>& ans,vector<int>row)
+    {
+        if(i >= nums.size())
+        {
+            ans.push_back(row);
+            return;
+        }
+        row.push_back(nums[i]);
+        rec(i + 1,nums,ans,row);
+        row.pop_back();
+        rec(i + 1,nums,ans,row);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        int n = nums.size();
-        for(int i = 0; i < (1 << n); i++)
-        {
-            vector<int>sub;
-            for(int j = 0; j < n; j++)
-            {
-                if(i & (1 << j)) sub.push_back(nums[j]);
-            }
-            ans.push_back(sub);
-        }
+        vector<int>row;
+        rec(0,nums,ans,row);
         return ans;
     }
 };
