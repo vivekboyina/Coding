3c4350b07fefc28dde2fd77930875b69f5217790
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-08 08:41:34 +0530

    ✅ Subsets II | N/A | C++ | Runtime: 6 ms (7.50%) | Memory: 17.5 MB | 2026-08-08

--- leetcode/n/a/0090-subsets-ii/solution.cpp
+++ leetcode/n/a/0090-subsets-ii/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void rec(int i,vector<int>nums,set<vector<int>>& st,vector<int>row)
+    void rec(int i,vector<int>nums,vector<vector<int>>& ans,vector<int>row)
     {
         if(i >= nums.size())
         {
-            st.insert(row);
+            ans.push_back(row);
             return;
         }
         row.push_back(nums[i]);
-        rec(i + 1,nums,st,row);
+        rec(i + 1,nums,ans,row);
         row.pop_back();
-        rec(i + 1,nums,st,row);
+        while(i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
+        rec(i + 1,nums,ans,row);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>st;
+        vector<vector<int>>ans;
         vector<int>row;
-        rec(0,nums,st,row);
-        vector<vector<int>>ans(st.begin(),st.end());
+        sort(nums.begin(),nums.end());
+        rec(0,nums,ans,row);
         return ans;
     }
 };
