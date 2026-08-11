d29d36b95d2fe3ee9dff6cd9f2ed3a998595584d
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-11 14:41:02 +0530

    ✅ Count Primes | N/A | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-11

---
+++ leetcode/n/a/0204-count-primes/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sieve(int n,vector<bool>& prms)
+    {
+        for(int i = 2; i*i <= n; i++)
+        {
+            if(prms[i] == true) for(int j = i*i; j <= n; j+=i) prms[j] = false;
+        }
+    }
+    int countPrimes(int n) {
+        if(n < 2) return 0;
+        vector<bool>prms(n,true);
+        prms[0] = false;
+        prms[1] = false;
+        sieve(n,prms);
+        int ans = 0;
+        for(bool i : prms) if(i) ans++;
+        return ans;
+    }
+};
