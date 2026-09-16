cfe1aad409d21baa5e3531cd11e262567db56621
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-09-16 18:39:49 +0530

    ✅ Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold | 1317 | C++ | Runtime: 0 ms (100.00%) | Memory: 59.3 MB | 2026-09-16

--- leetcode/1317/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/solution.cpp
+++ leetcode/1317/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/solution.cpp
@@ -5,12 +5,11 @@ public:
         int n = arr.size();
         for(int i = 0; i < k; i++) sum+=arr[i];
         int ans = 0;
-        if(sum/k > th) ans+=1;
+        if(sum/k >= th) ans+=1;
         for(int i = k; i < n; i++)
         {
             sum-=arr[i - k];
             sum+=arr[i];
-            cout << sum/k << endl;
             if((sum/k) >= th) ans+=1;
         }
         return ans;
