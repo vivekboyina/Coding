7ea8117a09ea386b7ccdd02d50d13b19a9492115
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-09-22 14:52:42 +0530

    ✅ N-Queens | N/A | C++ | Runtime: 2 ms (72.90%) | Memory: 11.3 MB | 2026-09-22

--- leetcode/n/a/0051-n-queens/solution.cpp
+++ leetcode/n/a/0051-n-queens/solution.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
-    bool valid(int &i,int &j,vector<string>&ans,int &n)
-    {
-        int ti = i;
-        int tj = j;
-        while(ti >= 0)
-        {
-            if(ans[ti][tj] == 'Q') return false;
-            ti--;
-        }
-        ti = i;
-        while(ti >= 0 && tj >= 0)
-        {
-            if(ans[ti][tj] == 'Q') return false;
-            ti--;
-            tj--;
-        }
-        ti = i;
-        tj = j;
-        while(ti >= 0 && tj < n)
-        {
-            if(ans[ti][tj] == 'Q') return false;
-            ti--;
-            tj++;
-        }
-        return true;
-    }
-    void bkt(vector<vector<string>>&chs,vector<string>&ans,int r,int &n)
+    bitset<30>cl,dl,dr;
+    void bkt(vector<vector<string>>&ans,vector<string>&chs,int r,int &n)
     {
         if(r >= n)
         {
-            chs.push_back(ans);
+            ans.push_back(chs);
             return;
         }
         for(int i = 0; i < n; i++)
         {
-            if(valid(r,i,ans,n))
+            if(!cl[i] && !dl[r + i] && !dr[r - i + n - 1])
             {
-                ans[r][i] = 'Q';
-                bkt(chs,ans,r + 1,n);
-                ans[r][i] = '.';
+                chs[r][i] = 'Q';
+                cl[i] = dl[r + i] = dr[r - i + n - 1] = 1;
+                bkt(ans,chs,r + 1,n);
+                chs[r][i] = '.';
+                cl[i] = dl[r + i] = dr[r - i + n - 1] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>chs;
-        vector<string>ans;
+        vector<vector<string>>ans;
+        vector<string>chs;
         string s = "";
         for(int i = 0; i < n; i++) s+='.';
-        for(int i = 0; i < n; i++) ans.push_back(s);
-        bkt(chs,ans,0,n);
-        return chs;
+        for(int i = 0; i < n; i++) chs.push_back(s);
+        bkt(ans,chs,0,n);
+        return ans;
     }
 };
