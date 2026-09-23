ed6e22231dd679e61b5ab00f9ab3a44533a0eaa1
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-09-23 20:59:35 +0530

    ✅ LITUP | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LITUP/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,k;
+	    cin >> n >> k;
+	    vector<int>cc(n);
+	    for(int i = 0; i < n; i++) cin >> cc[i];
+	    int ans = -1;
+	    for(int i = 0; i < k; i++)
+	    {
+	        int cv = 0;
+	        for(int j = i + 1; j < n; j++)
+	        {
+	            for(int c = 0; c >= i; i++)
+	        }
+	    }
+	}
+}
