0f135bbd75cd546a3eaddd2e671994b215173e9b
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-09-24 18:00:52 +0530

    ✅ Majority Element II | N/A | C++ | Runtime: 0 ms (100.00%) | Memory: 23.8 MB | 2026-09-24

--- leetcode/n/a/0229-majority-element-ii/solution.cpp
+++ leetcode/n/a/0229-majority-element-ii/solution.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int num1 = INT_MAX;
+        int num2 = INT_MAX;
         int cnt1 = 0;
         int cnt2 = 0;
-        int num1,num2;
-        int n = nums.size();
         for(int i : nums)
         {
-            if(cnt1 == 0 && i != num2)
+            if(i == num1) cnt1++;
+            else if(i == num2) cnt2++;
+            else if(cnt1 == 0)
             {
-                cnt1 = 1;
                 num1 = i;
+                cnt1++;
             }
-            else if(cnt2 == 0 && i != num1)
+            else if(cnt2 == 0)
             {
-                cnt2 = 1;
                 num2 = i;
+                cnt2++;
             }
-            else if(i == num1) cnt1+=1;
-            else if(i == num2) cnt2+=1;
             else
             {
-                cnt1-=1;
-                cnt2-=1;
+                cnt1--;
+                cnt2--;
             }
         }
-        cnt1 = cnt2 = 0;
+        cnt1 = 0;
+        cnt2 = 0;
         for(int i : nums)
         {
-            if(i == num1) cnt1+=1;
-            else if(i == num2) cnt2+=1;
+            if(i == num1) cnt1++;
+            else if(i == num2) cnt2++;
         }
-        vector<int>ans;
-        if(cnt1 > n/3) ans.push_back(num1);
-        if(cnt2 > n/3) ans.push_back(num2);
-        return ans;
+        int n = nums.size();
+        vector<int>tuf;
+        if(cnt1 > n/3) tuf.push_back(num1);
+        if(cnt2 > n/3) tuf.push_back(num2);
+        return tuf;
     }
 };
